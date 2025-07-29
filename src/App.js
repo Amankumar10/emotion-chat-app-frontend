d53a895fbabe3ffa9c87576db406a886fafe5e1a
Amankumar10 <amankumar02740@gmail.com>
2025-07-29 09:14:25 +0530

    account implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../src/page/Login';
+import Register from '../src/page/Resister';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md text-center">
-        <h1 className="text-3xl font-bold text-blue-200 mb-4">
-          ✅ Tailwind is working!
-        </h1>
-        <p className="text-gray-700">
-          If you see this styled box, Tailwind setup is correct.
-        </p>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 

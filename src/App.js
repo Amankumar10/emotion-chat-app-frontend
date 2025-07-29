adb358ccf8537b3b0cf3184fdd837cd77277018e
Amankumar10 <amankumar02740@gmail.com>
2025-07-29 08:50:07 +0530

    first commit with tailwind setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md text-center">
+        <h1 className="text-3xl font-bold text-blue-200 mb-4">
+          ✅ Tailwind is working!
+        </h1>
+        <p className="text-gray-700">
+          If you see this styled box, Tailwind setup is correct.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
